94f498bff0b4e39d7be82266e6f538ae9c6c1126
App Academy Student <student@appacademy.io>
2017-11-12 17:35:18 -0800

    Autocomplete filters names based on query

--- react-widget/frontend/autocomplete.jsx
+++ react-widget/frontend/autocomplete.jsx
@@ -3,29 +3,38 @@ import React from 'react';
 class AutoComplete extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
+    // console.log(props);
+    this.allNames = props.names;
     this.state = {inputValue: "", possibleNames: props.names};
     this.handleChange = this.handleChange.bind(this);
   }
+
   handleChange(e) {
     e.preventDefault();
-    console.log(e.state.value);
-    const possibleNames = this.state.possibleNames;
     const inputValue = e.target.value;
-    console.log(this.state.possibleNames);
-
-    // console.log(e.target.value);
-    // throw 'asdf';
-    //Based off input string cull down the list of names
-    // props.names
-    this.setState({inputValue});
-    console.log('Inputvalue: ', inputValue);
-    // setState()
+    console.log(this.allNames);
+    // this.state.possibleNames = "johnny";
+    // const temp = this.allNames[0];
+    let result = [];
+    this.allNames.forEach((name) => {
+      if (name.toLowerCase().indexOf(inputValue) >= 0) {
+        result.push(name);
+        console.log(name, inputValue, name.indexOf(inputValue));
+      }
+    });
+    const possibleNames = result;
+    // this.setState( (possibleNames) => {
+    //   console.log('here');
+    //   console.log(possibleNames);
+    //   this.state.possibleNames = 'johnny';
+    // });
+    this.setState({inputValue, possibleNames});
   }
   render() {
     return (
       <div>
         <input type="text" value={this.state.inputValue} onChange={this.handleChange}></input>
+        <p>{this.state.possibleNames}</p>
       </div>
     );
   }
